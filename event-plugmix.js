91dacece377edf4364ac00832245b8b4dea48419
rektide <rektide@voodoowarez.com>
2014-10-04 20:20:00 -0400

    Initial module-walk.

---
+++ event-plugmix.js
@@ -0,0 +1,64 @@
+var co= require('co'),
+  glob= require('co-glob'),
+  path= require('path')
+
+function*globSlot(o, slot){
+	for(var p of o[slot]){
+		yield co(p)
+	}
+}
+
+function*dir(dirs){
+	for(var dir of dirs){
+		yield fs.readDir(dir)
+	}
+}
+
+function startsWith(phrases){
+	if(phrases.length)
+		return (function*winnow(exprs){
+			EXPR: for(var expr of exprs)
+				for(var phrase of phrases)
+					if(expr.startsWith(phrase)){
+						yield expr
+						continue EXPR
+					}
+		})
+	else
+		return (function*winnow(exprs){
+			for(var expr of exprs)
+				if(expr.startsWith(phrases)){
+					yield expr
+					continue
+				}
+		})
+}
+
+function startsWith(phrases){
+	if(phrases.length)
+		return (function*winnow(exprs){
+			EXPR: for(var expr of exprs)
+				for(var phrase of phrases)
+					if(expr.contains(phrase)){
+						yield expr
+						continue EXPR
+					}
+		})
+	else
+		return (function*winnow(exprs){
+			for(var expr of exprs)
+				if(expr.contains(phrases)){
+					yield expr
+					continue
+				}
+		})
+}
+
+function*find(module, prefix){
+	var paths= yield globSlot( module, 'paths')
+	var dirs= yield dir(paths)
+
+	var prefix= prefix|| require('path').basename(module.id)
+	var matches= yield startsWith(prefix)
+	return matches
+}
