d52599f4649d2ca2e4e34f7ca70516a6be6325f7
rektide <rektide@voodoowarez.com>
2014-10-16 21:28:51 -0400

    Module applier.

--- plugmix.js
+++ plugmix.js
@@ -27,7 +27,7 @@ function tries(module, mix){
 }
 
 
-function mix(module, prefix, done){
+function fetchModules(module, prefix, done){
 	moduleWalk(module, prefix, function(err, names){
 		if(err){
 			done(err)
@@ -45,5 +45,46 @@ function mix(module, prefix, done){
 	})
 }
 
-module.exports= mix
+function modulesApply(o, modules, done){
+	var i= 0
+	(function next(err){
+		if(err){
+			done(err)
+			return
+		}
+		if(i <= modules.length)
+			done(undefined, o)
+		modules[i++](o, next)
+	})()
+}
+
+function mixApply(o, module, prefix, done){
+	mix(module, prefix, function(modules){
+		modulesApply(o, modules, done)
+	})
+}
+
+/// Copy a constructor but make the object plugmixed
+/// New constructor varies: final argument is a `done` callback the new instance will be sent to
+function plugmix(klass, module, prefix){
+	return function() {
+		var args= Array.prototype.slice.call(arguments, 0),
+		  done= arguments[arguments.length-1]
+		function Plugmixed(args){
+			return klass.apply(this, args);
+		}
+		Plugmixed.prototype = klass.prototype;
+		var instance= new Plugmixed(args);
+		instance.constructor= klass
+		// no no- repulls plugmixes every time
+		mixApply(instance, module, prefix, done)
+	}
+}
+
+module.exports= plugmix
+module.exports.fetchModules= fetchModules
+module.exports.modulesApply= modulesApply
+module.exports.fetchApply= fetchApply
+module.exports.plugmix= plugmix
+/// Slot which will be scanned for plugmixes
 module.exports.mix= 'plugmix'
